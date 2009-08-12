5a65978e0fe33503be5bb47cb326f29af44104a4
Hector Garcia <hector@nomadblue.com>
2009-08-12 22:52:48 +0200

    Each view has its homonimous function that fills out the context. This way external apps will be able to use their logic without to mess around with the view. An example would be a wrapper view for any of the nomadblog views, to add functionality or something.

--- nomadblog/views.py
+++ nomadblog/views.py
@@ -10,58 +10,71 @@ from nomadblog.models import Post, Category
 
 
 def _get_extra_filters(username=None):
+    """
+    Set a queryset param to filter by blog, if username is
+    provided -- that is, Nomadblog is being used in a multi-user layout.
+    """
     extra_filters = {}
     if username is not None:
         u = get_object_or_404(User, username=username)
         extra_filters['blog__user'] = u
     return extra_filters
 
-def show_post(request, category, slug, username=None, render_response=True,
-              context=None, template='nomadblog/show_post.html', extra_content={}):
-    extra_filters = _get_extra_filters(username)
-    try:
-        cat = Category.objects.get(name=category)
-        post = Post.objects.get(category=cat, slug=slug,
-                                status=Post.PUBLIC_STATUS, **extra_filters)
-    except ObjectDoesNotExist:
-        raise Http404
-    ctxt_dict = {'post': post}
-    ctxt_dict.update(extra_content)
-    context = RequestContext(request, ctxt_dict) if context is None \
-        else context.update(ctxt_dict)
-    if not render_response:
-        return context
-    return render_to_response(template, {}, context_instance=context)
-
-def list_posts(request, username=None, render_response=True, context=None,
-               template='nomadblog/list_posts.html'):
+def _list_posts(request, context=None, username=None):
+    """Populates context with values required by list_posts view"""
     extra_filters = _get_extra_filters(username)
     posts = Post.objects.filter(status=Post.PUBLIC_STATUS,\
         **extra_filters).order_by('-pub_date')
     ctxt_dict = {'posts': posts}
     context = RequestContext(request, ctxt_dict) if context is None \
         else context.update(ctxt_dict)
-    if not render_response:
-        return context
-    return render_to_response(template, {}, context_instance=context)
+    return context
 
-def list_categories(request, username=None, render_response=True, context=None,
-                    template='nomadblog/list_categories.html'):
-    categories = Category.objects.all()
+def _show_post(request, category, slug, context=None, username=None):
+    extra_filters = _get_extra_filters(username)
+    cat = get_object_or_404(Category, name=category)
+    post = get_object_or_404(Post, category=cat, slug=slug,
+                             status=Post.PUBLIC_STATUS, **extra_filters)
+    ctxt_dict = {'post': post}
+    context = RequestContext(request, ctxt_dict) if context is None \
+        else context.update(ctxt_dict)
+    return context
+
+def _list_categories(request, context=None, username=None):
+    categories = Category.objects.all() # TODO: only cats from username
     ctxt_dict = {'categories': categories}
     context = RequestContext(request, ctxt_dict) if context is None \
         else context.update(ctxt_dict)
-    if not render_response:
-        return context
-    return render_to_response(template, {}, context_instance=context)
+    return context
 
-def list_posts_by_category(request, category,
-                           username=None, render_response=True, context=None,
-                           template='nomadblog/list_posts_by_category.html'):
+def _list_posts_by_category(request, category, context=None, username=None):
     extra_filters = _get_extra_filters(username)
     cat = get_object_or_404(Category, name=category)
     posts = Post.objects.filter(category=cat, status=Post.PUBLIC_STATUS, \
         **extra_filters).order_by('-pub_date')
-    return render_to_response(template, {'posts': posts, 'category': cat.name},
-        context_instance=RequestContext(request))
+    ctxt_dict = {'posts': posts, 'category': category}
+    context = RequestContext(request, ctxt_dict) if context is None \
+        else context.update(ctxt_dict)
+    return context
+
+def list_posts(request, username=None, template='nomadblog/list_posts.html'):
+    """A list of posts, orderen in reverse chronological order."""
+    context = _list_posts(request, username=username)
+    return render_to_response(template, {}, context)
+
+def show_post(request, category, slug, username=None,
+              template='nomadblog/show_post.html'):
+    context = _show_post(request, category, slug, username=username)
+    return render_to_response(template, {}, context)
+
+def list_categories(request, username=None,
+                    template='nomadblog/list_categories.html'):
+    context = _list_categories(request, username=username)
+    return render_to_response(template, {}, context)
+
+def list_posts_by_category(request, category,
+                           username=None, render_response=True, context=None,
+                           template='nomadblog/list_posts_by_category.html'):
+    context = _list_posts_by_category(request, category, username=username)
+    return render_to_response(template, {}, context)
 
