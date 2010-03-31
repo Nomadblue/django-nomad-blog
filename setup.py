0302d7865c8632c2df1686def36017240a340f14
Hector Garcia <hector@nomadblue.com>
2010-03-31 18:42:10 +0200

    Fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from distutils.core import setup
 
 doc_dir = os.path.join(os.path.dirname(__file__), 'docs')
 long_desc_file = os.path.join(doc_dir, 'overview.rst')
+version_num = __import__('nomadblog').__version__ 
 
 setup(
     name='django-nomadblog',
-    version=__import__('nomadblog').__version__,
+    version=version_num,
     description='A minimalist Django blogging system.',
     long_description=open(long_desc_file).read(),
     author='Hector Garcia',
     author_email='hector@nomadblue.com',
     url='http://nomadblue.com/projects/django-nomadblog/',
-    download_url='http://bitbucket.org/nabucosound/django-nomadblog/downloads/django-nomadblog-0.2.tar.gz',
+    download_url='http://bitbucket.org/nabucosound/django-nomadblog/downloads/django-nomadblog-%s.tar.gz' % version_num,
     packages=['nomadblog'],
     classifiers=[
       'Development Status :: 4 - Beta',
