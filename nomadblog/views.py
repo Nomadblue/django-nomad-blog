a081a7d155653e331c364e0ea5e0b6c80631a7dc
Hector Garcia <hector@nomadblue.com>
2010-05-14 20:43:23 +0200

    Added a new model param in views that allows passing a different model other than Post (in most cases, a subclass of Post). Added a MIT license to the project.

--- nomadblog/views.py
+++ nomadblog/views.py
@@ -20,21 +20,22 @@ def _get_extra_filters(username=None):
         extra_filters['blog__user'] = u
     return extra_filters
 
-def _list_posts(request, context=None, username=None):
+def _list_posts(request, model=Post, context=None, username=None):
     """Populates context with values required by list_posts view"""
     extra_filters = _get_extra_filters(username)
-    posts = Post.objects.filter(status=Post.PUBLIC_STATUS,\
+    posts = model.objects.filter(status=model.PUBLIC_STATUS,\
         **extra_filters).order_by('-pub_date')
     ctxt_dict = {'posts': posts}
     context = RequestContext(request, ctxt_dict) if context is None \
         else context.update(ctxt_dict)
     return context
 
-def _show_post(request, category, slug, context=None, username=None):
+def _show_post(request, category, slug, model=Post,
+               context=None, username=None):
     extra_filters = _get_extra_filters(username)
     cat = get_object_or_404(Category, name=category)
-    post = get_object_or_404(Post, category=cat, slug=slug,
-                             status=Post.PUBLIC_STATUS, **extra_filters)
+    post = get_object_or_404(model, category=cat, slug=slug,
+                             status=model.PUBLIC_STATUS, **extra_filters)
     ctxt_dict = {'post': post}
     context = RequestContext(request, ctxt_dict) if context is None \
         else context.update(ctxt_dict)
@@ -47,24 +48,27 @@ def _list_categories(request, context=None, username=None):
         else context.update(ctxt_dict)
     return context
 
-def _list_posts_by_category(request, category, context=None, username=None):
+def _list_posts_by_category(request, category, model=Post,
+                            context=None, username=None):
     extra_filters = _get_extra_filters(username)
     cat = get_object_or_404(Category, name=category)
-    posts = Post.objects.filter(category=cat, status=Post.PUBLIC_STATUS, \
+    posts = model.objects.filter(category=cat, status=model.PUBLIC_STATUS, \
         **extra_filters).order_by('-pub_date')
     ctxt_dict = {'posts': posts, 'category': category}
     context = RequestContext(request, ctxt_dict) if context is None \
         else context.update(ctxt_dict)
     return context
 
-def list_posts(request, username=None, template='nomadblog/list_posts.html'):
+def list_posts(request, model=Post, username=None,
+               template='nomadblog/list_posts.html'):
     """A list of posts, orderen in reverse chronological order."""
-    context = _list_posts(request, username=username)
+    context = _list_posts(request, model=model, username=username)
     return render_to_response(template, {}, context)
 
-def show_post(request, category, slug, username=None,
+def show_post(request, category, slug, model=Post, username=None,
               template='nomadblog/show_post.html'):
-    context = _show_post(request, category, slug, username=username)
+    context = _show_post(request, category, slug,
+                         model=model, username=username)
     return render_to_response(template, {}, context)
 
 def list_categories(request, username=None,
@@ -72,9 +76,10 @@ def list_categories(request, username=None,
     context = _list_categories(request, username=username)
     return render_to_response(template, {}, context)
 
-def list_posts_by_category(request, category,
+def list_posts_by_category(request, category, model=Post,
                            username=None, render_response=True, context=None,
                            template='nomadblog/list_posts_by_category.html'):
-    context = _list_posts_by_category(request, category, username=username)
+    context = _list_posts_by_category(request, category,
+                                      model=model, username=username)
     return render_to_response(template, {}, context)
 
