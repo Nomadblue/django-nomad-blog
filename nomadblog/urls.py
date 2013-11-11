9cffd831aaf1aff1dd3f655c4336c57831fef700
Hector Garcia <hector@nomadblue.com>
2013-11-11 17:31:05 -0300

    Fix ImportError: No module named defaults (dj 1.6)

--- nomadblog/urls.py
+++ nomadblog/urls.py
@@ -1,26 +1,11 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, include, url
 from nomadblog.views import PostList
 
 
-urlpatterns = patterns('nomadblog.views',
-    # List blog posts
+urlpatterns = patterns(
+    'nomadblog.views',
     url('^$', PostList.as_view(), name='list_posts'),
-    # Show single post, category + slug based URL
-    url(
-        regex=r'^(?P<category_slug>[-\w]+)/(?P<post_slug>[-\w]+)/$',
-        view='show_post',
-        name='show_post',
-    ),
-    # List categories
-    url(
-        regex=r'^categories/list/all/$',
-        view='list_categories',
-        name='list_categories',
-    ),
-    # List posts by category
-    url(
-        regex=r'^(?P<category>[-\w]+)/$',
-        view='list_posts_by_category',
-        name='list_posts_by_category',
-    ),
+    url(regex=r'^(?P<category_slug>[-\w]+)/(?P<post_slug>[-\w]+)/$', view='show_post', name='show_post'),
+    url(regex=r'^categories/list/all/$', view='list_categories', name='list_categories'),
+    url(regex=r'^(?P<category>[-\w]+)/$', view='list_posts_by_category', name='list_posts_by_category'),
 )
